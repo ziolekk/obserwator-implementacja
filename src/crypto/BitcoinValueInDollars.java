0570bcf979eeab9337f9b4c7131116a7a5afe0b4
Wiktor Ziółek <ziolkowskiwiktor99@gmail.com>
2018-12-22 22:25:35 +0100

    Added BitcoinValueInDollars and implemented observable methods

--- src/crypto/BitcoinValueInDollars.java
+++ src/crypto/BitcoinValueInDollars.java
@@ -1,4 +1,38 @@
 package crypto;
 
-public class BitcoinValueInDollars {
+import java.util.ArrayList;
+import java.util.Date;
+
+public class BitcoinValueInDollars implements Observable {
+
+    private double valueInDollars;
+    private Date dateOfChange;
+    private List<Observer> registeredObservers = new ArrayList<>();
+
+    public BitcoinValueInDollars(double valueInDollars) {
+        this.valueInDollars = valueInDollars;
+        this.dateOfChange = new Date();
+    }
+
+    @Override
+    public void registerObserver(Observer observer) {
+        registeredObservers.add(observer);
+    }
+
+    @Override
+    public void unregisterObserver(Observer observer) {
+        registeredObservers.remove(observer);
+    }
+
+    @Override
+    public void notifyObservers() {
+        for (Observer observer: registeredObservers) {
+            observer.update(this);
+        }
+    }
+
+    public void changeValue(double valueInDollars) {
+        this.valueInDollars = valueInDollars;
+        this.dateOfChange = new Date();
+    }
 }
